35e782d40eed6ca49065ce9e724664ab0cb606e7
Juan Pardo <juan.pardo@metrica.es>
2020-09-29 10:31:11 +0200

    best saying changed

--- src/main/java/com/metricalab/saying/configuration/PersistentContext.java
+++ src/main/java/com/metricalab/saying/configuration/PersistentContext.java
@@ -23,26 +23,26 @@ public class PersistentContext {
 	public InMemorySaying buildSaying() {
 
 		final List<SayingDTO> listsaying = new ArrayList<>();
-		listsaying.add(new SayingDTO(Long.valueOf(1), "A caballo regalado no le mires el diente", 3,
+		listsaying.add(new SayingDTO((long) 1, "A caballo regalado no le mires el diente", 3,
 				ConstantsData.SPRING_BOOT_MEMORY));
-		listsaying.add(new SayingDTO(Long.valueOf(2), "A falta de pan, buenas son tortas", 4,
+		listsaying.add(new SayingDTO((long) 2, "A falta de pan, buenas son tortas", 4,
 				ConstantsData.SPRING_BOOT_MEMORY));
-		listsaying.add(new SayingDTO(Long.valueOf(3), "A la arrogancia en el pedir, la virtud del no dar", 12,
+		listsaying.add(new SayingDTO((long) 3, "A la arrogancia en el pedir, la virtud del no dar...", 12,
 				ConstantsData.SPRING_BOOT_MEMORY));
 		listsaying
-				.add(new SayingDTO(Long.valueOf(4), "A la tercera va la vencida", 3, ConstantsData.SPRING_BOOT_MEMORY));
+				.add(new SayingDTO((long) 4, "A la tercera va la vencida", 3, ConstantsData.SPRING_BOOT_MEMORY));
 		listsaying.add(
-				new SayingDTO(Long.valueOf(5), "A palabras necias, oídos sordos", 5, ConstantsData.SPRING_BOOT_MEMORY));
+				new SayingDTO((long) 5, "A palabras necias, oídos sordos", 5, ConstantsData.SPRING_BOOT_MEMORY));
 		listsaying.add(
-				new SayingDTO(Long.valueOf(6), "Al pan, pan, y al vino, vino", 11, ConstantsData.SPRING_BOOT_MEMORY));
-		listsaying.add(new SayingDTO(Long.valueOf(7), "Al perro flaco, todo se le vuelven pulgas", 1,
+				new SayingDTO((long) 6, "Al pan, pan, y al vino, vino", 11, ConstantsData.SPRING_BOOT_MEMORY));
+		listsaying.add(new SayingDTO((long) 7, "Al perro flaco, todo se le vuelven pulgas", 1,
 				ConstantsData.SPRING_BOOT_MEMORY));
-		listsaying.add(new SayingDTO(Long.valueOf(8), "Al que Dios se la dé, San Pedro se la bendiga", 9,
+		listsaying.add(new SayingDTO((long) 8, "Al que Dios se la dé, San Pedro se la bendiga", 9,
 				ConstantsData.SPRING_BOOT_MEMORY));
-		listsaying.add(new SayingDTO(Long.valueOf(9), "Antes se coge al mentiroso que al cojo", 8,
+		listsaying.add(new SayingDTO((long) 9, "Antes se coge al mentiroso que al cojo", 8,
 				ConstantsData.SPRING_BOOT_MEMORY));
 		listsaying.add(
-				new SayingDTO(Long.valueOf(10), "Burro grande, ande o no ande", 2, ConstantsData.SPRING_BOOT_MEMORY));
+				new SayingDTO((long) 10, "Burro grande, ande o no ande", 2, ConstantsData.SPRING_BOOT_MEMORY));
 
 		final InMemorySaying inMemorySaying = new InMemorySaying();
 		inMemorySaying.setSaying(listsaying);
